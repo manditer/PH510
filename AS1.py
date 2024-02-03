bcf8387b6133b5ee8b6b665c05b36452c62d115d
Mandi <80163446+manditer@users.noreply.github.com>
2024-02-03 21:50:19 +0000

    Pi correct to around 13 sig figs

--- AS1.py
+++ AS1.py
@@ -1,37 +1,37 @@
 #!/bin/python3
 
 from mpi4py import MPI
+import numpy as np
 
 # MPI.Init()
 
 comm = MPI.COMM_WORLD
-
 nproc = comm.Get_size()
 rank = comm.Get_rank()
 
+N = 300000000
+I = np.array(0.0, dtype = np.double)
+
 nworkers = nproc - 1
 
-N = 100000000
-delta = 1.0 / N
+def integrand(i):
+    x = (i+0.5) / N
+    I_estim = 4.0 / (1.0 + x*x)
+    return(I_estim)
+
+steps = np.linspace(0, N, nworkers+1)
+
+if rank != 0:
+    print('rank', rank, 'range', steps[rank-1], steps[rank])
+    #range doesnt include last value, stop not inlcuded
+    for i in range(int(steps[rank-1]), int(steps[rank])):
+        I += integrand(i)
 
 
-I = 0.0
-
-
-def integrand(x):
-    result = 4.0 / (1.0 + x*x)
-    return result
-
-
-for i in range(0, N):
-    x = (i+0.5) * delta
-    I =+ integrand(x)
-
-print('Rank ', rank, ' computes ', I)
-
-summa = comm.reduce(I, MPI.SUM, 0)
+print('Rank ',rank,' computes ', I)
+summa=comm.reduce(I/N, MPI.SUM, 0)
 
 if rank == 0:
-    print("Integral %.15f" % I)
+    print("Integral %.15f" % summa)
 
 # MPI.Finalize()
