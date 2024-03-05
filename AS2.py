c22598e2116eefaf1ced2fbca0dd0c04274120cf
Mandi <80163446+manditer@users.noreply.github.com>
2024-03-05 21:08:44 +0000

    As.2, var and mean fix, cleaning

--- AS2.py
+++ AS2.py
@@ -9,12 +9,12 @@ import time
 
 class MonteCarloIntegrationParallel:
 
-    def __init__(self, N, mean, lim_l, lim_u, function, dim, seed=10000):
+    def __init__(self, N, lim_l, lim_u, function, dim, seed=10000):
         self.N = N
-        self.mean = mean
         self.lim_l = lim_l
         self.lim_u = lim_u
         self.function = function
+        self.dim = dim
         self.seed = seed
 
         self.comm = MPI.COMM_WORLD
@@ -22,59 +22,62 @@ class MonteCarloIntegrationParallel:
         self.rank = self.comm.Get_rank()
         self.ss = np.random.SeedSequence(seed)
         self.child_ss = self.ss.spawn(self.nproc)
-        self.dim = dim
 
     def monte_carlo(self):
+        n_data_points = self.n_data_points()
+        random_positions, integrals_at_positions = self.random_positions(n_data_points)
+        integral, integral_std = self.integral_and_std(integrals_at_positions)
 
-        if self.rank == 0:
-            data_points = int(self.N / self.nproc) + (self.N % self.nproc)
-        else:
-            data_points = int(self.N / self.nproc)
+        pos_average = np.mean(random_positions, axis=0) / self.nproc
+        pos_variance = np.var(random_positions, axis=0) / self.nproc
 
-        integral = 0
-        integral_squared = 0
-        pos_average = np.zeros(self.dim)
-        # pos variance doesnt work yet. array of positions? could get too large. Make a function for it
-        pos_variance = np.zeros(self.dim)
+        return integral, integral_std / self.nproc, pos_average, pos_variance
 
-        grand_child_ss = self.child_ss[self.rank].spawn(data_points)
-
-        for i in range(data_points):
-            grand_grand_child_ss = grand_child_ss[i].spawn(self.dim)
-            for j in range(self.dim):
-                rng2 = np.random.default_rng(grand_grand_child_ss[j])
-                rand_points = rng2.uniform(self.lim_l, self.lim_u)
-
-            pos_average += rand_points / self.N
-            pos_variance += (rand_points - self.mean) ** 2 / self.N
-
-            point = self.function(rand_points)
-            integral += point
-            integral_squared += point ** 2
-
+    def integral_and_std(self, integrals_at_positions):
+        integrals_summed = np.sum(integrals_at_positions)
         integral_limits = (self.lim_u[0] - self.lim_l[0])
         for i in range(self.dim - 1):
             integral_limits *= (self.lim_u[i + 1] - self.lim_l[i + 1])
+        integral_estim = integral_limits * integrals_summed / self.N
+        integral_std = np.std(integrals_at_positions)
 
-        integral_estim = integral_limits * integral / self.N
-        # variance = 1/self.N * (integral_squared/self.N - integral/self.N)
-        return integral_estim, pos_average, pos_variance  # variance, mean
+        return integral_estim, integral_std
+
+    def random_positions(self, n_data_points):
+        positions = np.zeros((n_data_points, self.dim))
+        integrals_at_data_points = np.zeros(n_data_points)
+        grand_child_ss = self.child_ss[self.rank].spawn(n_data_points)
+        for i in range(n_data_points):
+            rng3 = np.random.default_rng(grand_child_ss[i])
+            rand_point = rng3.uniform(self.lim_l, self.lim_u)
+            for j in range(self.dim):
+                positions[i][j] = rand_point[j]
+            integrals_at_data_points[i] = self.function(rand_point)
+
+        return positions, integrals_at_data_points
+
+    def n_data_points(self):
+        if self.rank == 0:
+            data_points = int(self.N / (self.nproc)) + (self.N % self.nproc)
+        else:
+            data_points = int(self.N / (self.nproc))
+        return data_points
 
     def running_in_parallel(self):
-        I, pos_mean, pos_var = self.monte_carlo()
+        # comm gather
+        I, I_std, pos_mean, pos_var = self.monte_carlo()
         integral = self.comm.reduce(I, MPI.SUM, 0)
-        mean = self.comm.reduce(pos_mean, MPI.SUM, 0)
-        variance = self.comm.reduce(pos_var, MPI.SUM, 0)
+        integral_std = self.comm.reduce(I_std, MPI.SUM, 0)
+        pos_mean = self.comm.reduce(pos_mean, MPI.SUM, 0)
+        pos_var = self.comm.reduce(pos_var, MPI.SUM, 0)
 
-        return integral, mean, variance, self.rank
+        return integral, integral_std, pos_mean, pos_var, self.rank
 
 
 def func(x):
     return np.sin(x)
 
 
-# variance should be sigma**2
-# mean should be the same as the mean defined here, 0? position mean? or integral mean?
 dim = 6
 mean = np.ones(dim) * 0
 sigma = 1
@@ -88,13 +91,14 @@ def func2(points):
     return gauss
 
 
-sin_monte = MonteCarloIntegrationParallel(1000000, mean, mean - 4 * sigma, mean + 4 * sigma, func2, dim, seed=1900)
+sin_monte = MonteCarloIntegrationParallel(2500000, mean - 4 * sigma, mean + 4 * sigma, func2, dim, seed=1900)
 start_time = time.time()
-I, mean, var, rank = sin_monte.running_in_parallel()
+I, I_std, pos_mean, pos_var, rank = sin_monte.running_in_parallel()
 
 if rank == 0:
     time_taken = time.time() - start_time
-    print('Integral: %.5f' % I, '\nmean pos: ', np.round(mean, 5), '\nvariance pos:', np.round(var, 5))
+    print('Integral: %.3f' % I, ' +-  %.3f' % I_std, '\nmean pos: ', np.round(pos_mean, 3), '\nvariance pos:',
+          np.round(pos_var, 3))
     print('time taken %.2f' % time_taken)
 
 # MPI.Finalize()
